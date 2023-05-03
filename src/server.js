08cb6697bbaf79ff76e70fcc30cb1ccd7c823f21
TatoPirtakhia <tarassatato@gmail.com>
2023-05-03 19:05:25 +0400

    fix cors

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv"
 import connect from './database/mongo.js'
 import spaceRoute from './route/routes.js'
 import swaggerMiddleware from './middlewares/middleware.js'
-
+import cors from "cors"
 const app = express()
 dotenv.config()
 
 app.use(bodyParser.json())
 connect()
-
+app.use(cors());
 app.use("/images", express.static("public/storage"));
 app.use('/api',spaceRoute)
 app.use("/", ...swaggerMiddleware())
